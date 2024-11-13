8bfee243db5d2059be13ea421091a06001f47380
Pierre Gleize <gleizepierre3gmail.com>
2024-11-13 14:06:04 +0100

    firstcommit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+// const OWM_API_KEY = "36b33a58e4677ed2f59fd17f2d22f4dd";
+const api_key = process.env.OWM_API_KEY;
+
+router.get("/movies", async (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ movies: data.results });
+    });
+});
+
+module.exports = router;
