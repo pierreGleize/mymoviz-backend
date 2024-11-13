4c6accd18944f3cc83e760691d744b1574b95db9
Pierre Gleize <gleizepierre3gmail.com>
2024-11-13 15:24:36 +0100

    zeaefgzbd

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
 
-// const OWM_API_KEY = "36b33a58e4677ed2f59fd17f2d22f4dd"; ok
-const api_key = process.env.OWM_API_KEY;
+const OWM_API_KEY = "36b33a58e4677ed2f59fd17f2d22f4dd";
+// const api_key = process.env.OWM_API_KEY;
 
 router.get("/movies", async (req, res) => {
   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
