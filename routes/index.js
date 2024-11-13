3881273e691b8183d11977e416402828d0cfe026
Pierre Gleize <gleizepierre3gmail.com>
2024-11-13 15:26:13 +0100

    zeaefgzbd

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const OWM_API_KEY = "36b33a58e4677ed2f59fd17f2d22f4dd";
 // const api_key = process.env.OWM_API_KEY;
 
 router.get("/movies", async (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
       res.json({ movies: data.results });
